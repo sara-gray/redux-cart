c3be8eb235214ccf7e2ced079aeba2395c2089e0
Sara Gray <saragrayen@gmail.com>
2021-05-31 08:05:20 +0100

    first action

--- src/App.js
+++ src/App.js
@@ -1,20 +1,51 @@
-import React from "react";
+import React from 'react'
 // components
-import Navbar from "./components/Navbar";
-import CartContainer from "./components/CartContainer";
+import Navbar from './components/Navbar'
+import CartContainer from './components/CartContainer'
 // items
-import cartItems from "./cart-items";
+import cartItems from './cart-items'
 // redux stuff
 
-function App() {
-  // cart setup
+// stores data, think of state
+import { createStore } from 'redux'
 
-  return (
-    <main>
-      <Navbar />
-      <CartContainer cart={cartItems} />
-    </main>
-  );
+// dispatch method - send actions to the store
+// actions (objects) - MUST HAVE TYPRE PROPERTY - what kind of action
+// DON'T MUTATE THE STATE - redux built on IMMUTABILITY (copy)
+
+// intial store
+const initialStore = {
+	count: 78,
 }
 
-export default App;
+// reducer - function that is used to update state
+// two arguments - state, action
+// state - old state/steate before update
+// action - what happened/ what update
+// return updates or old state
+const reducer = (state, action) => {
+	console.log({ state, action })
+	if (action.type === 'DECREASE') {
+		return { count: state.count - 1 }
+	}
+
+	return state
+}
+
+// store
+const store = createStore(reducer, initialStore)
+store.dispatch({ type: 'DECREASE' })
+console.log(store.getState())
+
+function App() {
+	// cart setup
+
+	return (
+		<main>
+			<Navbar cart={store.getState()} />
+			<CartContainer cart={cartItems} />
+		</main>
+	)
+}
+
+export default App
