a367ddedcac8ab3d0fed82da75be82b9ee4da0c3
Sara Gray <saragrayen@gmail.com>
2021-05-31 10:02:14 +0100

    working on reducer functions

--- src/reducer.js
+++ src/reducer.js
@@ -4,10 +4,34 @@
 // action - what happened/ what update
 // return updates or old state
 
-// import { DECREASE, INCREASE } from './actions'
+import { CLEAR_CART, REMOVE, DECREASE, INCREASE } from './actions'
 
 const reducer = (state, action) => {
-	return state
+	const { type, payload } = action
+	switch (type) {
+		case CLEAR_CART:
+			return { ...state, cart: [] }
+
+		case DECREASE:
+			console.log('decrease')
+			return state
+
+		case INCREASE:
+			let tempCart = state.cart.map((item) => {
+				console.log(item)
+				return item
+			})
+			return { ...state, cart: tempCart }
+
+		case REMOVE:
+			return {
+				...state,
+				cart: state.cart.filter((item) => item.id !== payload.id),
+			}
+
+		default:
+			return state
+	}
 }
 
 export default reducer
